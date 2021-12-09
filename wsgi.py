a40e57efacfce7ccb081a81734d537597ed6ce4b
Valerian <v.levinski@gmail.com>
2021-12-09 15:48:59 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,4 @@
-#from app.main import app
-#from app.app import app
-#from app2.main import app
-#from app3.main import app
-#from app4.app_min_rel import app
-#from app4.main import app
-#from app5.main import app
+#!usr/bin/python
 from app6.main import app
 
 if __name__ == "__main__":
