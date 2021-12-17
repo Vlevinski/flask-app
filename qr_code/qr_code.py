30575d2264dc1802cff2a145aae67fd444c1e4f8
Vlevinski <v.levinski@gmail.com>
2021-12-17 12:51:21 +0200

    Create README.md

--- qr_code/qr_code.py
+++
@@ -1,5 +0,0 @@
-import pyqrcode
-import png
-url = pyqrcode.create('http://www.twitter.com/vlevinski')
-#url.png('vlevinski.png', scale=6, module_color=[0, 0, 0, 128], background=[0xff, 0xff, 0xcc])
-url.png('vlevinski.png', scale=6)
